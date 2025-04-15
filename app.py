ddd5686656ffbef5fe0b5ec08a973fd10053b786
KillerHyena <crunchvelvet72@gmail.com>
2025-04-15 23:49:18 +0530

    Initial commit

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def upload():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=5001, debug=False, threaded=True)
